c9e9591bc77bad8a36c59e5c035709e5a5b7409f
obaraelijah <elijahobara357@gmail.com>
2023-07-26 12:41:52 +0300

    LRU Caching fix pycodestyle

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -18,7 +18,7 @@ class LRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
+                lru_key, _ = self.cache_data.popitem(last=True)
                 print("DISCARD:", lru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
