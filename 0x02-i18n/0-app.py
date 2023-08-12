95068acd7ce674f83d2770f62f4579d9a5c671a4
obaraelijah <elijahobara357@gmail.com>
2023-08-12 16:09:34 +0300

    Hot fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,12 +5,15 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello():
+def get_index() -> str:
+    """The index page.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
