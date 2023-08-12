ecf2e7a5a234cafd1a4fa67915eece9ea9f60cda
obaraelijah <elijahobara357@gmail.com>
2023-08-12 16:00:17 +0300

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
