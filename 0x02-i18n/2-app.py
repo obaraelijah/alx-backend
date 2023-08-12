e5f1cf6814de1ae49eb5c6722a73c4cd7ea8b7a0
obaraelijah <elijahobara357@gmail.com>
2023-08-12 17:22:33 +0300

    locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+@app.route('/')
+def get_index() -> str:
+    """The index page.
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """ 
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
