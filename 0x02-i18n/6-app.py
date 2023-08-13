916b9d0116aca730e7604ade3e53d082a6029780
obaraelijah <elijahobara357@gmail.com>
2023-08-13 12:37:00 +0300

    internalization in python

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@app.route('/')
+def get_index() -> str:
+    """The index page.
+    """
+    return render_template('1-index.html')
+
+def get_user():
+    """returns a user dictionary or None if the ID cannot be found
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """_summary_
+    """
+    user = get_user()
+    g.user = user
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """ 
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
