eef495712b07a31ed1283d275d15350e8716d4d2
obaraelijah <elijahobara357@gmail.com>
2023-08-28 10:53:17 +0300

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+#!/usr/bin/npm dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+  });
+
+  client.on('error', (err) => {
+    console.log('Redis client not connected to the server:', err.toString());
+  });
